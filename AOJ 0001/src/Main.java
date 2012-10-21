63008cb7bd159a0a415920676fb38adc29b3914a
Kensho Aoki <kensho.aoki@gmail.com>
2012-10-21 17:31:31 +0900

    Edit AOJ 0001

--- AOJ 0001/src/Main.java
+++ AOJ 0001/src/Main.java
@@ -1,31 +1,40 @@
 import java.util.*;
 public class Main {
 	static Scanner sc = new Scanner(System.in);
-	static int[][] edge;
+	static int[] mountains;//山の情報を保存する配列
 	public static void main(String[] args) {
-		//System.out.println(1<<29);
-		//System.out.println(Integer.MAX_VALUE);
-		edge = new int[1000000][2];
-		for(int i = 0; i < edge.length; i++){
-			for(int j = 0; j < edge[0].length; j++){
-				edge[i][j] = 1<<29;
+		while(read()){
+			solve();
+		}
+	}
+	/**
+	 * read関数で入力の読み込みを行う
+	 */
+	static boolean read(){
+		if(!sc.hasNext())return false;
+		mountains = new int[10];
+		for(int i = 0; i < 10; i++){
+			mountains[i] = sc.nextInt();//int型の入力の読み込みはnextInt()で
+		}
+		return true;
+	}
+	/**
+	 * solve関数で計算し、出力する
+	 */
+	static void solve(){
+		//Arrays.sort(mountains);実は、以下の処理はこの1行でできた
+		for(int i = 0; i < 10; i++){
+			for(int j = i; j < 10; j++){
+				if(mountains[j] < mountains[i]){
+					int swap = mountains[i];
+					mountains[i] = mountains[j];
+					mountains[j] = swap;
+				}
 			}
 		}
-		//System.out.println(edge.length);
-		//System.out.println(edge[0].length);
-		String str = "AOKI";
-		String str2 = "AOKI";
-		str2 += "";
-		//String str2 =  new String("AOKI");
-		//str2 = str;
-		if(str == str2)System.out.println("OK");
-		StringBuffer sb = new StringBuffer("AOKI");
-		
-		System.out.println(sb.reverse());
-		System.out.println(str+str2);
-		
-		System.out.println(str.equals("AOKI"));
-		System.out.println(str.substring(0, 2));
+		System.out.println(mountains[9]);//C言語のprintf("\n");にあたるのがSystem.out.println();
+		System.out.println(mountains[8]);
+		System.out.println(mountains[7]);
 	}
 
 }
